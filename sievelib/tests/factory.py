93364ad5233afe1a7a0141fccbdffbe69f7aa1ef
Antoine Nguyen <tonio@ngyn.org>
2013-04-01 09:43:18 +0200

    New unit tests.

--- sievelib/tests/factory.py
+++ sievelib/tests/factory.py
@@ -14,6 +14,7 @@ class FactoryTestCase(unittest.TestCase):
         self.fs.addfilter("rule1",
                           [('Sender', ":is", 'toto@toto.com'),],
                           [("fileinto", 'Toto'),])
+        self.assertIsNot(self.fs.getfilter("rule1"), None)
         self.fs.tosieve(output)
         self.assertEqual(output.getvalue(), """require ["fileinto"];
 
@@ -24,6 +25,37 @@ if anyof (header :is "Sender" "toto@toto.com") {
 """)
         output.close()
 
+    def test_remove_filter(self):
+        self.fs.addfilter("rule1",
+                          [('Sender', ":is", 'toto@toto.com'),],
+                          [("fileinto", 'Toto'),])
+        self.assertIsNot(self.fs.getfilter("rule1"), None)
+        self.assertEqual(self.fs.removefilter("rule1"), True)
+        self.assertIs(self.fs.getfilter("rule1"), None)
+
+
+    def test_disablefilter(self):
+        """
+        FIXME: Extra spaces are written between if and anyof, why?!
+        """
+        self.fs.addfilter("rule1",
+                          [('Sender', ":is", 'toto@toto.com'),],
+                          [("fileinto", 'Toto'),])
+        self.assertIsNot(self.fs.getfilter("rule1"), None)
+        self.assertEqual(self.fs.disablefilter("rule1"), True)
+        output = cStringIO.StringIO()
+        self.fs.tosieve(output)
+        self.assertEqual(output.getvalue(), """require ["fileinto"];
+
+# Filter: rule1
+if false {
+    if     anyof (header :is "Sender" "toto@toto.com") {
+        fileinto "Toto";
+    }
+}
+""")
+        output.close()
+        self.assertEqual(self.fs.is_filter_disabled("rule1"), True)
 
 if __name__ == "__main__":
     unittest.main()
