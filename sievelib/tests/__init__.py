8d7d28df7f25073b9f9f8721522155bc96f9579a
Antoine Nguyen <tonio@ngyn.org>
2014-03-12 21:36:35 +0100

    Travis integration.

---
+++ sievelib/tests/__init__.py
@@ -0,0 +1,14 @@
+from sievelib.tests.test_parser import (
+    AdditionalCommands, ValidSyntaxes, ValidEncodings, InvalidSyntaxes,
+    LanguageRestrictions, DateCommands, VariablesCommands
+)
+from sievelib.tests.test_factory import (
+    FactoryTestCase
+)
+
+
+__all__ = [
+    'ValidSyntaxes', 'AdditionalCommands', 'ValidEncodings',
+    'InvalidSyntaxes', 'LanguageRestrictions', 'DateCommands',
+    'VariablesCommands', 'FactoryTestCase'
+]
