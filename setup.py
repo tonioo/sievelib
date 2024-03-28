5c6e4837330ed2099134bd7a825e2efd470f051f
Antoine Nguyen <tonio@ngyn.org>
2024-03-28 11:52:01 +0100

    Fixed test pypi upload

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ from os import path
 from setuptools import setup, find_packages
 
 
+def local_scheme(version):
+    """
+    Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
+    to be able to upload to Test PyPI
+    """
+    return ""
+
+
 if __name__ == "__main__":
     HERE = path.abspath(path.dirname(__file__))
     with io.open(path.join(HERE, "README.rst"), encoding="utf-8") as readme:
@@ -28,15 +36,16 @@ if __name__ == "__main__":
         keywords=["sieve", "managesieve", "parser", "client"],
         install_requires=[],
         setup_requires=["setuptools_scm"],
-        use_scm_version=True,
+        use_scm_version={"local_scheme": local_scheme},
         classifiers=[
             "Programming Language :: Python",
-            "Development Status :: 4 - Beta",
+            "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Topic :: Software Development :: Libraries :: Python Modules",
-            "Topic :: Communications :: Email :: Filters"
+            "Topic :: Communications :: Email :: Filters",
         ],
-        long_description=LONG_DESCRIPTION
+        python_requires=">=3.7",
+        long_description=LONG_DESCRIPTION,
     )
